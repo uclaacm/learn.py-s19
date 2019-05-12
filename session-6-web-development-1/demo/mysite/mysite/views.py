16d445b8dfea478b3c449c31e5cc398512504f97
Connie <connie90503@gmail.com>
2019-05-12 13:53:02 -0700

    templates

--- session-6-web-development-1/demo/mysite/mysite/views.py
+++ session-6-web-development-1/demo/mysite/mysite/views.py
@@ -1,18 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Views are Python functions that take a Web request and return a Web Response- in this case, an HttpResponse object
 # that contains the contents of the specified HTML page
 def home_page(request):
-    file = open("../../index.html")
-    html = file.read()
-    return HttpResponse(html)
+    return render(request, 'index.html')
 
 def dark_mode(request):
-    file = open("../../darkMode.html")
-    html = file.read()
-    return HttpResponse(html)
-
+    return render(request, 'darkMode.html')
+    
 def piktures(request):
-    file = open("../../piktures.html")
-    html = file.read()
-    return HttpResponse(html)
+    return render(request, 'piktures.html')
